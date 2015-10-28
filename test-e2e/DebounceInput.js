d31c1933469a14e50f5d698fc71268a9d0b184c1
Nik Butenko <nik@butenko.me>
2015-10-28 21:09:29 +1100

    Adapt e2e test

--- test-e2e/DebounceInput.js
+++ test-e2e/DebounceInput.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url('http://localhost:8080/')
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Component')
+      .assert.containsText('body', 'Test')
       .end();
   }
 };
