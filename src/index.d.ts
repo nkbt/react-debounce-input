09ef0681e0e17c26b55f0bcaaf8659efbc55a197
Igor Yegorov <iegoroff@gmail.com>
2018-02-02 22:58:31 +0300

    added type usage examples

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,63 +1,41 @@
 /*
- *  - usage with default 'input' element:
- *
- *      <DebounceInput
- *        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {}}
- *      />
- *
- *  - usage with 'textarea':
- *
- *      const DebouncedTextArea: Debounced<HTMLTextAreaElement> = DebounceInput;
- *      
- *      <DebouncedTextArea
- *        element={'textarea'}
- *        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {}}
- *      />
- *
- *  - usage with custom element that has 'value' and 'onChange' props:
- *
- *      interface MyComponentProps {
- *        value?: string | number;
- *        onChange: React.ChangeEventHandler<MyComponent>;
- *      }
- *
- *      class MyComponent extends React.Component<MyComponentProps> {}
- *
- *      const DebouncedMyComponent: Debounced<MyComponent> = DebounceInput;
- *
- *      <DebouncedMyComponent
- *        element={MyComponent}
- *        onChange={(e: React.ChangeEvent<MyComponent>) => {}}
- *      />
+ *  check usage examples in ./example/typescript-example.tsx 
  */
 
 import * as React from 'react';
 
-interface WrappedProps<T> {
+interface PropConstraints<T> {
   readonly value?: string | number;
   readonly onChange: React.ChangeEventHandler<T>;
 }
 
-export type Debounced<T> = React.ComponentType<DebounceInputProps<T>>;
-
-export interface DebounceInputProps<
-  Wrapped = HTMLInputElement
-> extends React.InputHTMLAttributes<Wrapped> {
-  readonly element?: string | React.ComponentType<WrappedProps<Wrapped>>;
+export type DebounceInputProps<
+  WrappedComponent = HTMLInputElement,
+  WrappedComponentProps = React.InputHTMLAttributes<HTMLInputElement>
+> = WrappedComponentProps & {
+  readonly element?: string | React.ComponentType<PropConstraints<WrappedComponent>>;
   readonly type?: string;
-  readonly onChange: React.ChangeEventHandler<Wrapped>;
-  readonly onKeyDown?: React.KeyboardEventHandler<Wrapped>;
-  readonly onBlur?: React.FocusEventHandler<Wrapped>;
+  readonly onChange: React.ChangeEventHandler<WrappedComponent>;
+  readonly onKeyDown?: React.KeyboardEventHandler<WrappedComponent>;
+  readonly onBlur?: React.FocusEventHandler<WrappedComponent>;
   readonly value?: string | number;
   readonly minLength?: number;
   readonly debounceTimeout?: number;
   readonly forceNotifyByEnter?: boolean;
   readonly forceNotifyOnBlur?: boolean;
-  readonly inputRef?: React.Ref<Wrapped>;
-}
+  readonly inputRef?: React.Ref<WrappedComponent>;
+};
 
-export declare class DebounceInput<
-  Wrapped = HTMLInputElement
-> extends React.PureComponent<DebounceInputProps<Wrapped>> {
+export declare class DebounceInput<C, P> extends React.PureComponent<DebounceInputProps<C, P>> {
 
 }
+
+export type Debounced<
+  WrappedComponent,
+  WrappedComponentProps
+> = React.ComponentType<DebounceInputProps<WrappedComponent, WrappedComponentProps>>;
+
+export type DebounceTextArea = Debounced<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>;
