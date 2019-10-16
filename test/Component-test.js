743f9b410361fc45bb18a6678e09b280c8571220
Nik Butenko <nik@butenko.me>
2019-10-16 14:29:16 +0800

    Upgrade

--- test/Component-test.js
+++ test/Component-test.js
@@ -1,6 +1,27 @@
-import test from 'tape';
-import {DebounceInput} from '../src/Component';
+const babel = require('@babel/register');
 
+babel({
+  babelrc: false,
+  plugins: [
+    require.resolve('@babel/plugin-proposal-object-rest-spread'),
+    require.resolve('@babel/plugin-proposal-class-properties')
+  ],
+  presets: [
+    require.resolve('@babel/preset-react'),
+    [require.resolve('@babel/preset-env'), {
+      targets: {
+        node: '10'
+      },
+      modules: 'commonjs',
+      loose: true
+    }]
+  ],
+  retainLines: true,
+  comments: false
+});
+
+const test = require('tape');
+const {DebounceInput} = require('../src/DebounceInput');
 
 test('DebounceInput', t => {
   t.ok(DebounceInput instanceof Function, 'should be function');
