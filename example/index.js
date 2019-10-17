7a44186f5275e1440372e491a853545f6b58b4dd
Nik Butenko <nik@butenko.me>
2019-10-17 09:20:59 +0800

    Wrap Example app in React.Strict

--- example/index.js
+++ example/index.js
@@ -9,4 +9,4 @@ const appRoot = document.createElement('div');
 
 appRoot.id = 'app';
 document.body.appendChild(appRoot);
-ReactDOM.render(<App />, appRoot);
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, appRoot);
