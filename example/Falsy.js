19ae0b838a6684996f0433b98d48a556bbb0bf67
Thibaut Rousseau <thibaut.rousseau.44@gmail.com>
2020-10-29 13:07:18 +0100

    lint

--- example/Falsy.js
+++ example/Falsy.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import {DebounceInput} from '../src';
 
-export class Falsy extends React.Component {
-  render() {
-    return (
-      <div>
-        <DebounceInput type="number" value={0} />
-      </div>
-    )
-  }
-}
+export const Falsy = () => (
+  <div>
+    <DebounceInput type="number" value={0} />
+  </div>
+);
